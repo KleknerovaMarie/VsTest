1e6996b9050c14af741a9aa4761c847916a44174
KleknerovaMarie <racanovamarie@gmail.com>
2024-12-02 08:54:15 +0100

    zkouška ulozeni

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print("Ahoj")
-print("1")
+name = ("bob", "ann", "mike", "liz")
+password = ("123", "pass123", "password123", "pass123")
+
+user = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123", }
+
+
+
+u_tab = input("user name:")
+if u_tab in name:
+        tab = input("password: ")
+        if user.get(name) == user.get(password):
+            print("you are here")
+        else:
+            print("wrong password")
+            
+else:
+        print("wrong user name")
+   
+
+
+
--- ukol2.py
+++ ukol2.py
@@ -35,3 +35,48 @@ print('Nová jména přidána: ', zamestnanci_a)
 zamestnanci_b = ['František', "Bruno",'Anna','Jakub','Klára',]
 print('Nová jména vložena:', zamestnanci_b)
                  
+
+vstupni_cisla = [1, 2, 3, 4, 5, 6, 7]
+vstupni_pismena = ["p", "ú", "s", "č", "p", "s", "n"]
+tyden = ('pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle')
+cislo_dne = 3
+den_tydne = tyden [cislo_dne - 1]
+if cislo_dne in vstupni_cisla:
+    print("Správná vstupní hodnota!")
+    if den_tydne.startswith(vstupni_pismena[cislo_dne - 1]):
+     
+        print("Správné písmeno")
+
+    else:
+        print("Špatné písmeno!")
+else:
+    print("Špatná vstupní hodnota!")
+
+print(den_tydne)
+
+
+
+
+user_email = {"email": "marek.parek@gmail.com"}
+
+# Vytvoř nový slovník "user_1"
+user_1 = dict()
+
+# Doplň klíče "name" a "surname" s hodnotami "Marek" a "Parek"
+user_1['name'] = 'Marek'
+user_1['surname'] = 'Parek'
+
+# Doplň slovník "user_1" o slovník "user_email"
+user_1.update(user_email)
+
+# Vypiš hodnoty ze slovníku "user_1" s doplňujícím textem
+print("User #01:", user_1)
+
+
+jmeno = 'Marek'
+heslo = '1234'
+uzivatel = {'Marek': '1234'}
+if uzivatel.get(jmeno) == heslo:
+    print("Ahoj", jmeno, "vítej v aplikaci! Pokračuj...")
+else:
+    print("Uživatelské jméno nebo heslo nejsou v pořádku!")
