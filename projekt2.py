732d0f67d4d201e25ea26671727f1c816ba10348
KleknerovaMarie <racanovamarie@gmail.com>
2025-01-28 15:56:38 +0100

    hotovo, před úpravama

---
+++ projekt2.py
@@ -0,0 +1,101 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Marie Kleknerová
+email: racanovamarie@gmail.com
+"""
+
+import random
+
+
+
+separator = "-" * 50
+print("Hi there")
+print(separator)
+print("I\'ve generated a random 4 digit number for you.\nLet\'s play a bulls and cows game.")
+print(separator)
+
+
+
+
+def control_random(random_number):
+    random_number = str(random_number)
+    if len(random_number) == len(set(random_number)):
+        return True
+            
+    else:
+        return False
+
+def generater_number(start, stop):
+    generate = random.randint(start, stop)
+    while control_random(generate) == False:
+        if control_random(generate) == False:
+            generate = random.randint(start, stop)
+        else:
+            break
+    return generate
+    
+
+def control_estimation(estimation:str):
+    if len(estimation) == 4 and estimation[0] != "0" and estimation.isdigit():
+        return True
+    else:
+        return False
+        
+def bulls_cows(number:str, random_n:str):
+    bull = 0
+    cow = 0
+    all_index = range(len(number))
+    for index_number in all_index:          
+        if number[index_number] == random_n[index_number]:
+            bull += 1
+        else:
+            continue
+
+    for i_number in all_index:
+        if number[i_number] in random_n:
+            cow += 1
+        else:
+            continue
+    cow -= bull
+    bul = "bull:" if bull < 2 else "bulls:"
+    cov = "cow:" if cow < 2 else "cows:"
+    print(bul, bull, cov, cow )
+    return bull, cow
+    
+    
+
+statistics = 0
+play_estimate = None
+rand_number = generater_number(1000, 9999)
+rand_number = str(rand_number)
+print(rand_number)
+print("Enter a number:")
+print(separator)
+while play_estimate != rand_number:
+    play_estimate = input(">>> ")
+    statistics += 1
+    
+    if control_estimation(play_estimate) == False:
+        print("Wrong number.")
+        print(separator)
+    else:
+        bulls_cows(play_estimate, rand_number)
+        print(separator)
+guess = "guess!" if statistics == 1 else "guesses!"
+print("Correct, you\'ve guessed the right number \nin", statistics, guess)
+print(separator)
+print("That's amazing!")
+
+
+    
+    
+      
+            
+        
+
+    
+
+
+
+
